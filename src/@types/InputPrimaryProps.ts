7e339a70179c39b9ebfef2ddede88a40491aaae5
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-07 10:28:39 -0300

    feat: types

---
+++ src/@types/InputPrimaryProps.ts
@@ -0,0 +1,5 @@
+export interface InputPrimaryProps{
+    type: string;
+    placeholder: string;
+    onChange: (e: string) => void;
+}
---
+++ src/@types/UserLogin.ts
@@ -0,0 +1,5 @@
+export interface UserLoginProps{
+    name?: string,
+    email: string,
+    password: string
+}
