4fc9b63353b897c70efbb3381868b722a3def20b
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-11 20:21:01 -0300

    feat: Page NewOrder

--- src/app/(mobile)/mesa/[...params]/page.tsx
+++ src/app/(mobile)/mesa/[...params]/page.tsx
@@ -1,12 +1,108 @@
-import React from 'react'
+'use client'
+import { CategoryProps } from '@/@types/CategoryProps'
+import { ProductProps } from '@/@types/ProductProps'
+import { TableProps } from '@/@types/TableProps'
+import { SelectCategory } from '@/components/Select/SelectCategory'
+import { SelectProduct } from '@/components/Select/SelectProducts'
+import { Button } from '@/components/ui/button'
+import { api } from '@/lib/api'
+import React, { useEffect, useState } from 'react'
 
 const Table = ({params} : {params: {params: string}}) => {
 
     const id = params.params[0]
 
+    const [table, setTable] = useState<TableProps | null>()
+    const [allCategories, setAllCategories] = useState<CategoryProps[]>([])
+    const [idCategory, setIdCategory] = useState<string>('')
+
+    const [productId, setProductId] = useState<string>('')
+    
+    const [productsSelectedByIdCategory, setProductsSelectedByIdCategory] = useState<ProductProps[] | null>()
+
+    const [qtd, setQtd] = useState<number>(1)
+
+    useEffect(() => {
+      async function getTable(){
+        const response = await api.get(`/api/table`, {
+          params: {
+            id: id
+          }
+        })
+        setTable(response.data)
+        console.log(response.data)
+      }
+
+      async function getCategories(){
+        const response = await api.get('/api/category')
+        setAllCategories(response.data)
+        console.log(response.data)
+      }
+      getCategories()
+      getTable()
+      
+    },[id])
+
+
+    useEffect(() => {
+      if (idCategory === '') return
+      async function getAllProductsByIdCategory(){
+        const response = await api.get('/api/product', {
+          params: {
+            idCategory: idCategory
+          }
+        })
+
+        setProductsSelectedByIdCategory(response.data)
+        console.log(response.data)
+      }
+      getAllProductsByIdCategory()
+    },[idCategory])
+
+    useEffect(() => {
+      if (isNaN(qtd)) {
+        setQtd(1)
+      }
+    },[qtd])
+
+
+    async function handleRegisterProductInTable(e : React.MouseEvent<HTMLButtonElement> | React.FormEvent<HTMLFormElement>){
+      e.preventDefault()
+      if (!productId || !qtd) return
+      try{
+        const response = await api.post('/api/order', {
+          idTable: table?.idTable,
+          idProduct: productId,
+          quantity: qtd,
+          tableId: table?.id
+        })
+
+        console.log(response)
+        setProductId('')
+        setQtd(1)
+      }catch{
+        console.log('erro')
+      }
+    }
+
   return (
-    <div>
-        Page Mesa: {id}
+    <div className='px-8 mt-20 flex flex-col gap-5'>
+        <h1 className='text-2xl font-bold'>Mesa {table?.idTable}</h1>
+        <div className='flex flex-col gap-4'>
+          <SelectCategory placeholder='Selecione a categoria...' category={idCategory} categories={allCategories} setCategory={setIdCategory} />
+          {productsSelectedByIdCategory && productsSelectedByIdCategory.length > 0 && (
+            <SelectProduct setProduct={setProductId} product={productId} placeholder='Escolha o produto...' products={productsSelectedByIdCategory} />
+          )}
+          {productsSelectedByIdCategory?.length === 0 && <p>Nenhum produto cadastrado nessa categoria</p>}
+        </div>
+        <div className='flex gap-4'>
+          <p className='w-[50%] text-center font-bold text-xl'>Quantidade</p>
+          <input className='w-full py-1.5 rounded-lg text-center text-black' value={qtd} type='number' onChange={(e) => setQtd(parseInt(e.target.value))} />
+        </div>
+        <div className='flex gap-4'>
+          <Button onClick={() => setQtd(qtd + 1)} variant={'secondary'} className='w-[40%] text-white font-bold text-2xl bg-blue-300 rounded-lg flex items-center justify-center'>+</Button>
+          <Button onClick={handleRegisterProductInTable} className='w-full bg-green-500 hover:bg-green-900'>Avançar</Button>
+        </div>
     </div>
   )
 }
--- src/app/(mobile)/novo-pedido/page.tsx
+++ src/app/(mobile)/novo-pedido/page.tsx
@@ -1,9 +1,13 @@
+import { FormNewOrder } from '@/components/Form/FormNewOrder'
 import React from 'react'
 
 const NewOrder = () => {
   return (
-    <div>
-      Page NewOrder
+    <div className='h-screen w-full flex flex-col items-center justify-center'>
+      <h1 className='text-center text-2xl font-bold mb-4'>Novo pedido</h1>
+      <div className='w-[85%]'>
+        <FormNewOrder />
+      </div>
     </div>
   )
 }
