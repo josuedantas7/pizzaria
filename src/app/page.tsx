cd9805e47ace5cbf358d4587d8e193c694bb301a
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-03-12 18:58:54 -0300

    hotfix: redirect hompage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Header from "@/components/Header/Header";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions)
+
+  if (!session || !session?.user) {
+    redirect('/login')
+  } else {
+    redirect('/pedidos')
+  }
+
   return (
     <main>
       <Header />
-      Home page
     </main>
   );
 }
